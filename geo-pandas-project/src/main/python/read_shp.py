069fa2b24c4f89af2875bd4520493e26f7dbf317
meenzoon <rlaalswns523@gmail.com>
2023-10-23 22:56:58 +0900

    [MOD] shp 읽기 방법 수정
    1. 특정 필드 기준 필터링 메소드 추가
    2. intersect 연산을 통해 속한 부분만 검사 부분 추가

--- geo-pandas-project/src/main/python/read_shp.py
+++ geo-pandas-project/src/main/python/read_shp.py
@@ -1,17 +1,32 @@
+from string import Template
+
 import geopandas as gpd
-import pandas as pd
 import os
+from geojson import Feature, dump
+from shapely import Polygon, intersects
 
 path = '/Users/meenzoon/Downloads/'
-file = 'sig_20230729/sig.shp'
-shp_file = os.path.join(path, file)
+sig_file = 'sig_20230729/sig.shp'
+shp_file = os.path.join(path, sig_file)
 # shp 파일 읽기
 sgg = gpd.read_file(shp_file)
 # shp 파일 기본 좌표계 정보 저장
 sgg.crs = {'init': 'epsg:5179'}
 
-print(sgg.crs)
+output_file = os.path.join(path, 'sip.json')
+f = open(output_file, 'w')
 # 파일 좌표계 변경
-sgg = sgg.to_crs(epsg=4326)
+jinju_sig = sgg[sgg['SIG_CD'] == '48170'].to_crs(epsg=4326)
 
-print(sgg)
+polygon = Polygon(((127.86837, 35.35689), (127.86837, 35.04432), (128.38336, 35.04432), (128.38336, 35.35689), (127.86837, 35.35689)))
+
+for i in range(0, len(jinju_sig)):
+    row = jinju_sig.iloc[i]
+    geom = row['geometry']
+
+    intersects(polygon, geom)
+
+    feat = Feature(geometry=row['geometry'], properties={"SIG_CD": row[0]})
+
+    f.write('{"index": {"_index": "sip"}}' + '\n')
+    dump(feat, f)
